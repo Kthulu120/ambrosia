427c50c4e0ad6b05639df0ef0ad0db25ef5c8f8e
Kthulu120 <troysmith@schedulenav.com>
2019-05-23 09:33:36 -0500

    Add Library to routes

--- app/Routes.js
+++ app/Routes.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router';
 import routes from './constants/routes';
 import App from './containers/App';
 import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
 import TitleBar from './components/TitleBar/TitleBar';
+import LibraryHome from './containers/Library/LibraryHome'
 
 export default () => (
   <App>
     <TitleBar/>
     <Switch>
-      <Route path={routes.COUNTER} component={CounterPage} />
+      <Route path={routes.LIBRARY} component={LibraryHome} />
       <Route path={routes.HOME} component={HomePage} />
     </Switch>
   </App>
