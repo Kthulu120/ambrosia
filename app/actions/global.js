c4375b9bd8eb97e312276277dabbae2ae6f1e02a
Kthulu120 <Troy.L.Smith-1@ou.edu>
2019-10-28 12:06:04 -0500

    add GQL id of viewer

--- app/actions/global.js
+++ app/actions/global.js
@@ -4,12 +4,20 @@ import type { GetState, Dispatch } from '../reducers/types';
 export const SET_ACESS_TOKEN = 'SET_ACESS_TOKEN';
 export const SET_REFRESH_TOKEN = 'SET_REFRESH_TOKEN';
 export const LOAD_GLOBAL_ALL= 'SET_GLOBAL_ALL';
+export const SET_USER_GQL_ID = 'SET_USER_GQL_ID';
 
 
 export function setAcsTkn(access_token: string) {
   return {
     type: SET_ACESS_TOKEN,
     access_token
+  };
+}
+
+export function setUserGQLId(newId: string) {
+  return {
+    type: SET_USER_GQL_ID,
+    newId
   };
 }
 
